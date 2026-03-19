392ba028e7025b8d7b8d628a63c9c6cd56b368b9
Abhishek yadav <yadavabhishek1529@gmail.com>
2026-03-19 10:16:24 +0530

    components concepts done

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Navbar />
+      <Card />
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className='card'>
+        <h1>Sarthak Sharma</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, vitae!</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
