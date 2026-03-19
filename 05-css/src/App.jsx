bdf3cd6c324287e73c2430cfe35f461c3eb794c6
Abhishek yadav <yadavabhishek1529@gmail.com>
2026-03-19 13:39:07 +0530

    module css concept done

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './components/button/Button'
+import Header from './components/header/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Button />
+    </div>
+  )
+}
+
+export default App
---
+++ 05-css/src/components/button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+const Button = () => {
+  return (
+    <div className={styles.btn}>
+        This is a Button
+    </div>
+  )
+}
+
+export default Button
