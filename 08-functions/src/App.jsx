e9c541ef8a3de23819ae36f311fee0736ac1882b
Abhishek yadav <yadavabhishek1529@gmail.com>
2026-03-29 18:06:08 +0530

    functions concept done

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,66 @@
+ /*import React from 'react'
+
+const App = () => {
+
+  function inputChanging(val){
+    console.log(val)
+  }
+  return (
+    <div>
+      <input onChange={function(elem){
+        inputChanging(elem.target.value)
+      }}
+      type='text' placeholder='Enter name' />
+      <button>explore this</button>
+    </div>
+  )
+}
+
+export default App  
+*/
+/*
+import React from 'react'
+
+const App = () => {
+
+
+  return (
+  <div>
+    <div onMouseMove={(elem)=>{
+      console.log(elem.clientX)
+  }} className='box'>
+
+    </div>
+  </div>  
+  )
+}
+
+export default App
+*/
+
+import React from 'react'
+
+const App = () => {
+
+  const pageScrolling = (elem) => {
+     if(elem>0){
+        console.log("sidha scrolling")
+      }else{
+        console.log("ulta scrolling")
+      }
+    // console.log("Page Scrolling...at speen",elem)
+  }
+
+  return (
+    <div onWheel={(elem) => {
+
+      pageScrolling(elem.deltaY);
+    }}>
+      <div className='page1'></div>
+      <div className='page2'></div>
+      <div className='page3'></div>
+    </div>
+  )
+}
+
+export default App
