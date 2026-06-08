773da724e5bf421e112299752a3053dfa8835f82
Abhishek yadav <yadavabhishek1529@gmail.com>
2026-06-08 15:59:08 +0530

    practicing tailwind css concept

--- 06-tailwind/src/App.jsx
+++ 06-tailwind/src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='h-screen w-full bg-gray-900'>App</div>
+    <div className='flex flex-col items-center justify-center space-y-6 mt-2'>
+      <div className='h-16 w-16 rounded-full bg-blue-500'></div>
+      <div className='h-16 w-16 rounded-full bg-orange-500'></div>
+      <div className='h-16 w-16 rounded-full bg-green-500'></div>
+    </div>
   )
 }
 
